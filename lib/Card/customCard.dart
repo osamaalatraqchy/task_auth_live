d5ac6017d3623eb075ac001b9c1735a8bb1d5c0e
osama <osamaalatraqchy@gmail.com>
2020-05-21 21:47:00 +0300

    Initial commit

---
+++ lib/Card/customCard.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:taskauthlive/task.dart';
+import '../task.dart';
+
+class CustomCard extends StatelessWidget {
+  CustomCard({@required this.title, this.description});
+
+  final title;
+  final description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Column(
+              children: <Widget>[
+                Text(title),
+                FlatButton(
+                    child: Text("See More"),
+                    onPressed: () {
+
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TaskPage(
+                                  title: title, description: description)));
+                    }),
+              ],
+            )));
+  }
+}
