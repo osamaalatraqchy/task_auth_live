f13e7a5127ca7c29bc47ed5ab403e7b9b40daa70
osama <osamaalatraqchy@gmail.com>
2020-08-01 13:25:03 +0300

    i add another commit

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'Card/customCard.dart';
 // i edit this code.....
+//this is by osama
 class HomePage extends StatefulWidget {
   HomePage({ this.title, this.uid}); //update this to include the uid in the constructor
   final String title;
